ff93429974e160c26a42055aab5d2b8a2148f8ce
ishansasika <ishansasika@gmail.com>
2025-12-24 21:05:49 +0530

    feat: Add App component and entry points

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <div class="page-container">
+    <Header />
+    <div class="content-wrapper">
+      <RouterView />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<style>
+@import './App.css';
+</style>
